b300a7437380f092df3cb821a00b64557bb81799
kilo52 <phil.gaiser@raven-computing.com>
2023-02-08 22:57:50 +0100

    Added isolated build and test integration with Docker for Python installable script project type

--- python/01_installable_script/source/test.sh
+++ python/01_installable_script/source/test.sh
@@ -9,21 +9,37 @@ Tests the ${{VAR_PROJECT_NAME}} program.
 ${USAGE}
 
 Options:
+${{VAR_SCRIPT_TEST_ISOLATED_OPT}}
 
-${{VAR_SCRIPT_TEST_LINT_HELP}}
+  [--lint]          Perform static code analysis with a linter.
 
-  [-?|--help] Show this help message.
+  [--no-virtualenv] Do not use a virtual environment for the tests.
+
+  [-?|--help]       Show this help message.
 EOS
 )
 
 # Arg flags
-${{VAR_SCRIPT_TEST_LINT_ARG}}
+${{VAR_SCRIPT_BUILD_ISOLATED_ARGFLAG}}
+ARG_LINT=false;
+ARG_NO_VIRTUALENV=false;
 ARG_SHOW_HELP=false;
+
+${{VAR_SCRIPT_BUILD_ISOLATED_ARGARRAY}}
 
 # Parse all arguments given to this script
 for arg in "$@"; do
   case $arg in
-${{VAR_SCRIPT_TEST_LINT_ARG_PARSE}}
+${{VAR_SCRIPT_BUILD_ISOLATED_ARGPARSE}}
+    --lint)
+    ARG_LINT=true;
+${{VAR_SCRIPT_BUILD_ISOLATED_ARGARRAY_ADD}}
+    shift
+    ;;
+    --no-virtualenv)
+    ARG_NO_VIRTUALENV=true;
+    shift
+    ;;
     -\?|--help)
     ARG_SHOW_HELP=true;
     shift
@@ -49,7 +65,27 @@ fi
 if ! source "setup.sh"; then
   exit 1;
 fi
-${{VAR_SCRIPT_TEST_LINT_CODE}}
+
+${{VAR_SCRIPT_TEST_ISOLATED_MAIN}}
+
+if [[ $ARG_NO_VIRTUALENV == false ]]; then
+  # Setup and activate virtual environment
+  if ! setup_virtual_env; then
+    exit 1;
+  fi
+fi
+
+# Check linter flag
+if [[ $ARG_LINT == true ]]; then
+  if ! command -v "pylint" &> /dev/null; then
+    logE "Could not find requirement 'pylint'";
+    exit 1;
+  fi
+  logI "Performing static code analysis";
+  # Add project root to sys.path. Needed for namespace packages
+  pylint --init-hook="sys.path.append('.')" "$_PROJECT_SRC_PACKAGE_MAIN";
+  exit 0;
+fi
 
 logI "Running unit tests";
 ${_PYTHON_EXEC} -m unittest;
