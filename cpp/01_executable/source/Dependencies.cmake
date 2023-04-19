5bc1d950b301d6bb2f6fffb788bc5a1354581719
kilo52 <phil.gaiser@raven-computing.com>
2023-04-19 19:45:02 +0200

    Updated dependency versions in Cpp-related project source templates

--- cpp/01_executable/source/Dependencies.cmake
+++ cpp/01_executable/source/Dependencies.cmake
@@ -3,6 +3,6 @@ include(cmake/DependencyUtil.cmake)
 dependency(
     DEPENDENCY_NAME         gtest
     DEPENDENCY_RESOURCE     google/googletest
-    DEPENDENCY_VERSION      release-1.12.1
+    DEPENDENCY_VERSION      v1.13.0
     DEPENDENCY_SCOPE        TEST
 )
--- cpp/03_server-poco/source/Dependencies.cmake
+++ cpp/03_server-poco/source/Dependencies.cmake
@@ -3,12 +3,12 @@ include(cmake/DependencyUtil.cmake)
 dependency(
     DEPENDENCY_NAME         poco
     DEPENDENCY_RESOURCE     pocoproject/poco
-    DEPENDENCY_VERSION      poco-1.12.2-release
+    DEPENDENCY_VERSION      poco-1.12.4-release
 )
 
 dependency(
     DEPENDENCY_NAME         gtest
     DEPENDENCY_RESOURCE     google/googletest
-    DEPENDENCY_VERSION      release-1.12.1
+    DEPENDENCY_VERSION      v1.13.0
     DEPENDENCY_SCOPE        TEST
 )
