50466f4b852e5498ee9696d87a7ba5b2ef4cd323
kilo52 <phil.gaiser@raven-computing.com>
2024-04-21 12:24:12 +0200

    Updated Poco version.

--- cpp/03_server-poco/source/Dependencies.cmake
+++ cpp/03_server-poco/source/Dependencies.cmake
@@ -3,7 +3,7 @@ include(cmake/DependencyUtil.cmake)
 dependency(
     DEPENDENCY_NAME         poco
     DEPENDENCY_RESOURCE     pocoproject/poco
-    DEPENDENCY_VERSION      poco-1.12.4-release
+    DEPENDENCY_VERSION      poco-1.13.3-release
 )
 
 ${{INCLUDE:cpp/cmake/DependenciesCommon.cmake}}
