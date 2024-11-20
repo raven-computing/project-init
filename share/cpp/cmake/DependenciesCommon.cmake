b2d654fc0e3e33dd834249c56f33bddd47bd90e8
kilo52 <phil.gaiser@raven-computing.com>
2024-11-20 21:55:42 +0100

    Updated Gtest dependency version.

--- share/cpp/cmake/DependenciesCommon.cmake
+++ share/cpp/cmake/DependenciesCommon.cmake
@@ -1,6 +1,6 @@
 dependency(
     DEPENDENCY_NAME         gtest
     DEPENDENCY_RESOURCE     google/googletest
-    DEPENDENCY_VERSION      v1.15.0
+    DEPENDENCY_VERSION      v1.15.2
     DEPENDENCY_SCOPE        TEST
 )
