8aba7f35504454383718067e40a010ab9539c0ca
kilo52 <phil.gaiser@raven-computing.com>
2025-03-02 11:42:03 +0100

    Updated Unity dependency version for C projects.

--- share/c/cmake/DependenciesCommon.cmake
+++ share/c/cmake/DependenciesCommon.cmake
@@ -1,6 +1,6 @@
 dependency(
     DEPENDENCY_NAME         unity
     DEPENDENCY_RESOURCE     ThrowTheSwitch/Unity
-    DEPENDENCY_VERSION      v2.6.0
+    DEPENDENCY_VERSION      v2.6.1
     DEPENDENCY_SCOPE        TEST
 )
