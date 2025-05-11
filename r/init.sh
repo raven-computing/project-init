5c5332379c83528747408561fdd5bbb89518e80e
kilo52 <phil.gaiser@raven-computing.com>
2025-05-11 11:44:36 +0200

    Added support for R 4.5.0 language standard.
    Signed-off-by: kilo52 <phil.gaiser@raven-computing.com>

--- r/init.sh
+++ r/init.sh
@@ -118,6 +118,7 @@ add_lang_version "4.1.0" "R 4.1.0";
 add_lang_version "4.2.0" "R 4.2.0";
 add_lang_version "4.3.0" "R 4.3.0";
 add_lang_version "4.4.0" "R 4.4.0";
+add_lang_version "4.5.0" "R 4.5.0";
 
 # Let the user choose an R project type
 select_project_type "r" "R";
