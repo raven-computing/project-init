14be3dda283182626895a3477156bd4e52e24102
kilo52 <phil.gaiser@raven-computing.com>
2025-06-11 00:31:59 +0200

    Updated Poco dependency version.
    Signed-off-by: kilo52 <phil.gaiser@raven-computing.com>

--- cpp/03_server-poco/source/Dependencies.cmake
+++ cpp/03_server-poco/source/Dependencies.cmake
@@ -3,7 +3,7 @@ include(cmake/DependencyUtil.cmake)
 dependency(
     DEPENDENCY_NAME         poco
     DEPENDENCY_RESOURCE     pocoproject/poco
-    DEPENDENCY_VERSION      poco-1.14.1-release
+    DEPENDENCY_VERSION      poco-1.14.2-release
 )
 
 ${{INCLUDE:cpp/cmake/DependenciesCommon.cmake}}
