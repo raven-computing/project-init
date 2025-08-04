b563ed0ded31ab32e791c485a4ca17054708abbc
kilo52 <phil.gaiser@raven-computing.com>
2025-08-04 19:55:25 +0200

    Added test coverage support for C++ library project template.
    Signed-off-by: kilo52 <phil.gaiser@raven-computing.com>

--- cpp/02_library/source/test.sh
+++ cpp/02_library/source/test.sh
@@ -9,6 +9,8 @@ Tests the ${{VAR_PROJECT_NAME}} library.
 ${USAGE}
 
 Options:
+
+  [--coverage] Report code coverage metrics for the test runs.
 ${{VAR_SCRIPT_TEST_ISOLATED_OPT}}
 
   [-?|--help]  Show this help message.
@@ -16,6 +18,7 @@ EOS
 )
 
 # Arg flags
+ARG_COVERAGE=false;
 ${{VAR_SCRIPT_BUILD_ISOLATED_ARGFLAG}}
 ARG_SHOW_HELP=false;
 
@@ -24,6 +27,11 @@ ${{VAR_SCRIPT_BUILD_ISOLATED_ARGARRAY}}
 # Parse all arguments given to this script
 for arg in "$@"; do
   case $arg in
+    --coverage)
+    ARG_COVERAGE=true;
+${{VAR_SCRIPT_BUILD_ISOLATED_ARGARRAY_ADD}}
+    shift
+    ;;
 ${{VAR_SCRIPT_BUILD_ISOLATED_ARGPARSE}}
     -\?|--help)
     ARG_SHOW_HELP=true;
@@ -66,6 +74,16 @@ if ! command -v "ctest" &> /dev/null; then
   echo "ERROR: Please make sure that CMake and CTest are correctly installed";
   exit 1;
 fi
+if [[ $ARG_COVERAGE == true ]]; then
+  if ! command -v "lcov" &> /dev/null; then
+    echo "ERROR: Could not find command 'lcov' to generate test coverage data";
+    exit 1;
+  fi
+  if ! command -v "genhtml" &> /dev/null; then
+    echo "ERROR: Could not find command 'genhtml' to generate test coverage report";
+    exit 1;
+  fi
+fi
 
 # Check if the build dir is empty
 if [ -z "$(ls -A build)" ]; then
@@ -75,6 +93,17 @@ if [ -z "$(ls -A build)" ]; then
   if (( $? != 0 )); then
     exit $?;
   fi
+fi
+
+COV_INCL_PATH="$PWD";
+BUILD_DIR_COV_DATA="cov";
+FILE_COV_DATA_MERGED="merged.info";
+BUILD_DIR_COV_REPORT="coverage_report";
+
+if [[ $ARG_COVERAGE == true ]]; then
+  # Remove previously collected test coverage data
+  rm -rf "build/${BUILD_DIR_COV_DATA}" "build/${BUILD_DIR_COV_REPORT}";
+  find . -name '*.gcda' -delete;
 fi
 
 cd "build";
@@ -99,4 +128,41 @@ export UBSAN_OPTIONS="halt_on_error=1:print_stacktrace=1";
 
 # Run tests with CTest
 ctest --output-on-failure --build-config "$BUILD_CONFIGURATION";
-exit $?;
+ctest_status=$?;
+
+if (( ctest_status == 0 )); then
+  if [[ $ARG_COVERAGE == true ]]; then
+    echo "Collecting coverage data";
+    mkdir "$BUILD_DIR_COV_DATA";
+    if ! lcov --quiet --directory . --capture \
+              --include "${COV_INCL_PATH}"'/src/*' \
+              --output-file "${BUILD_DIR_COV_DATA}/${FILE_COV_DATA_MERGED}"; then
+      echo "Failed to collect test coverage data";
+      exit 1;
+    fi
+    echo "Generating test coverage report";
+    genhtml_opt_args=();
+    genhtml_version=($(genhtml --version |grep -o -e '[0-9.]'));
+    genhtml_version="${genhtml_version[0]}";
+    if (( genhtml_version >= 2 )); then
+      genhtml_opt_args+=("--header-title");
+      genhtml_opt_args+=("${{VAR_PROJECT_NAME}} Test Coverage");
+      genhtml_opt_args+=("--footer");
+      genhtml_opt_args+=("Copyright © ${{VAR_COPYRIGHT_YEAR}} ${{VAR_PROJECT_ORGANISATION_NAME}}");
+    fi
+    if ! genhtml --quiet --output-directory "$BUILD_DIR_COV_REPORT" \
+                 --prefix "$COV_INCL_PATH" \
+                 --title "${{VAR_PROJECT_NAME}} Test Coverage" \
+                 "${genhtml_opt_args[@]}" \
+                 "${BUILD_DIR_COV_DATA}/${FILE_COV_DATA_MERGED}"; then
+      echo "Failed to generate test coverage report";
+      exit 1;
+    fi
+    report_label="build/${BUILD_DIR_COV_REPORT}/index.html";
+    report_url="file://${COV_INCL_PATH}/${report_label}";
+    report_link="\e]8;;${report_url}\e\\\\${report_label}\e]8;;\e\\\\";
+    echo -e "Wrote HTML report to $report_link";
+  fi
+fi
+
+exit $ctest_status;
