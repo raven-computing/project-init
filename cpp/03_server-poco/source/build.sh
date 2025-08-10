c4bc8a3687e2f5ff522c56bb7ee0191efe3a9070
kilo52 <phil.gaiser@raven-computing.com>
2025-08-10 11:35:49 +0200

    Added test coverage support to C++ server and desktop app project templates.
    Signed-off-by: kilo52 <phil.gaiser@raven-computing.com>

--- cpp/03_server-poco/source/build.sh
+++ cpp/03_server-poco/source/build.sh
@@ -15,6 +15,8 @@ Options:
   [--config]      Only execute the build configuration step. This option will skip
                   the build step.
 
+  [--coverage]    Enable code coverage instrumentation. Should only be used with debug builds.
+
   [--debug]       Build the application with debug symbols and with
                   optimizations turned off.
 ${{VAR_SCRIPT_BUILD_DOCS_OPT}}
@@ -37,6 +39,7 @@ EOS
 # Arg flags
 ARG_CLEAN=false;
 ARG_CONFIG=false;
+ARG_COVERAGE=false;
 ARG_DEBUG=false;
 ${{VAR_SCRIPT_BUILD_DOCS_ARGFLAG}}
 ${{VAR_SCRIPT_BUILD_ISOLATED_ARGFLAG}}
@@ -57,6 +60,11 @@ for arg in "$@"; do
     ;;
     --config)
     ARG_CONFIG=true;
+${{VAR_SCRIPT_BUILD_ISOLATED_ARGARRAY_ADD}}
+    shift
+    ;;
+    --coverage)
+    ARG_COVERAGE=true;
 ${{VAR_SCRIPT_BUILD_ISOLATED_ARGARRAY_ADD}}
     shift
     ;;
@@ -153,6 +161,7 @@ fi
 
 BUILD_TESTS="ON";
 BUILD_WITH_SANITIZERS="OFF";
+BUILD_WITH_COVERAGE="OFF";
 
 if [[ $ARG_SKIP_TESTS == true ]]; then
   BUILD_TESTS="OFF";
@@ -160,13 +169,21 @@ fi
 if [[ $ARG_SANITIZERS == true ]]; then
   BUILD_WITH_SANITIZERS="ON";
 fi
+if [[ $ARG_COVERAGE == true ]]; then
+  BUILD_WITH_COVERAGE="ON";
+  if [[ $ARG_SKIP_TESTS == true ]]; then
+    echo "Warning: Unable to automatically generate test coverage reports" \
+         "when test builds are skipped";
+  fi
+fi
 
 # CMake: Configure
 if [[ $ARG_SKIP_CONFIG == false ]]; then
   cmake -DCMAKE_BUILD_TYPE="$BUILD_CONFIGURATION" \
         -DBUILD_SHARED_LIBS="$BUILD_SHARED_LIBS" \
         -D${{VAR_PROJECT_NAME_UPPER}}_BUILD_TESTS="$BUILD_TESTS" \
-        -D${{VAR_PROJECT_NAME_UPPER}}_USE_SANITIZERS="$BUILD_WITH_SANITIZERS" .. ;
+        -D${{VAR_PROJECT_NAME_UPPER}}_USE_SANITIZERS="$BUILD_WITH_SANITIZERS" \
+        -D${{VAR_PROJECT_NAME_UPPER}}_BUILD_TEST_COVERAGE="$BUILD_WITH_COVERAGE" ..;
 
   config_status=$?;
   if (( config_status != 0 )); then
