852a4a2724ed81147bcd7fd45f69478c7bc26fd7
kilo52 <phil.gaiser@raven-computing.com>
2025-10-11 20:31:52 +0200

    Improved dependency() function.
    Removed unnecessary variable path append.
    
    Signed-off-by: kilo52 <phil.gaiser@raven-computing.com>

--- share/cpp/cmake/DependencyUtil.cmake
+++ share/cpp/cmake/DependencyUtil.cmake
@@ -449,11 +449,9 @@ function(dependency)
 
     # Check if a build file needs to be included
     if(DEFINED DEP_ARGS_DEPENDENCY_BUILD_FILE)
-        set(DEPENDENCY_BUILD_FILE "${CMAKE_CURRENT_SOURCE_DIR}/")
-        string(
-            APPEND
+        set(
             DEPENDENCY_BUILD_FILE
-            "${DEP_ARGS_DEPENDENCY_BUILD_FILE}"
+            "${CMAKE_CURRENT_SOURCE_DIR}/${DEP_ARGS_DEPENDENCY_BUILD_FILE}"
         )
         if(EXISTS "${DEPENDENCY_BUILD_FILE}")
             include("${DEPENDENCY_BUILD_FILE}")
