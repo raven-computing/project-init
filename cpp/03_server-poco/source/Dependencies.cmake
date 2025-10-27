9f09d8d64f3dfea0cad269eb2dc0898095521060
kilo52 <phil.gaiser@raven-computing.com>
2025-10-27 20:12:42 +0100

    Improved dependency handling for C++ server application.
    Changed to use new cache variable for linking Poco targets.
    
    Signed-off-by: kilo52 <phil.gaiser@raven-computing.com>

--- cpp/03_server-poco/source/Dependencies.cmake
+++ cpp/03_server-poco/source/Dependencies.cmake
@@ -4,6 +4,7 @@ dependency(
     DEPENDENCY_NAME         poco
     DEPENDENCY_RESOURCE     pocoproject/poco
     DEPENDENCY_VERSION      poco-1.14.2-release
+    DEPENDENCY_LINK_TARGETS Poco::XML Poco::JSON Poco::Net Poco::Util
 )
 
 ${{INCLUDE:cpp/cmake/DependenciesCommon.cmake}}
