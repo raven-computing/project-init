396d8081db0708eea9ab2d40cf2a9ebcf495c877
Phil Gaiser <phil.gaiser@raven-computing.com>
2026-04-03 11:28:20 +0700

    Updated Java Quickstart source template.
    Signed-off-by: Phil Gaiser <phil.gaiser@raven-computing.com>

--- share/java/Standalone_Main.java
+++ share/java/Standalone_Main.java
@@ -5,7 +5,7 @@
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("${{VAR_PROJECT_SLOGAN_STRING}}");
     }
 
