ecef0dff0a914a462792bf166e8b796ef955c01d
Phil Gaiser <phil.gaiser@raven-computing.com>
2026-07-01 22:17:55 +0800

    Improved Python standalone main source template.
    Removed pointless argc parameter.
    
    Signed-off-by: Phil Gaiser <phil.gaiser@raven-computing.com>

--- share/python/standalone_main.py
+++ share/python/standalone_main.py
@@ -3,10 +3,10 @@
 import sys
 
 
-def main(argc: int, argv: list[str]) -> int:
+def main(argv: list[str]) -> int:
     print("${{VAR_PROJECT_SLOGAN_STRING}}")
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main(len(sys.argv), sys.argv))
+    sys.exit(main(sys.argv))
